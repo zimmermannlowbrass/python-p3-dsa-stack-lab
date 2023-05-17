7cb820fb31cc19c7a3ea9b9511667a51c9e561c4
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-17 18:58:29 -0500

    completed labwork

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,32 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if not self.full():
+            return self.items.append(item)
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items.pop()
 
     def peek(self):
-        pass
+        return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        for x in range(len(self.items)):
+            if self.items[x] == target:
+                return len(self.items) - x -1
+        return -1
